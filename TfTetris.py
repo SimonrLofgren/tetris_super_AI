7f063f6bbdecda56f8400508ee4a0bb20e967722
Simon Löfgren <simonrlofgren@hotmail.com>
2021-05-03 10:27:07 +0200

    init commit

--- TfTetris.py
+++ TfTetris.py
@@ -12,8 +12,8 @@ for step in range(5000):
     if done:
         state = env.reset()
     state, reward, done, info = env.step(env.action_space.sample())
-    #grayimg = cv2.cvtColor(state, cv2.COLOR_RGB2GRAY)
-    #cv2.imshow('ComWin', grayimg)
+    grayimg = cv2.cvtColor(state, cv2.COLOR_RGB2GRAY)
+    cv2.imshow('ComWin', grayimg)
     env.render()
 
 env.close()
