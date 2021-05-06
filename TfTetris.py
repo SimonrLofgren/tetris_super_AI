df06a17a32708e29338ffeb60fd6a9a0fd10981a
Henrik Liman <Alpner93@gmail.com>
2021-05-06 11:01:51 +0200

    /8 pix

--- TfTetris.py
+++ TfTetris.py
@@ -9,7 +9,7 @@ import numpy as np
 import tensorflow
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import Dense
-from tensorflow.keras.optimizers import Adam
+from tensorflow.python.keras.optimizers import Adam
 
 
 class Agent:
@@ -18,7 +18,7 @@ class Agent:
         self.number_of_action = number_of_actions
         self.learning_rate = 0.1
         self.epsilon = 0.1
-        self.epsilon_min = 0.1
+        self.epsilon_min = 0.2
         self.epsilon_decay = 0.9
         self.batch_size = 512
         self.training_start = 1000
@@ -29,8 +29,9 @@ class Agent:
     def build_model(self):
 
         model = Sequential()
+
         model.add(Dense(1, input_dim=self.state_input_size))
-        model.add(Dense(30720))
+        model.add(Dense(480))
         model.add(Dense(60))
         model.add(Dense(24))
         model.add(Dense(self.number_of_action))
@@ -64,8 +65,8 @@ class Agent:
             if done[i]:
                 target[i][action[i]] = reward[i]
             else:
-                target[i][action[i]] = reward[i] + self.discount_factor * (np.amax(target_val[i]))
-
+                target[i][action[i]] = reward[i] + self.discount_factor * (
+                    np.amax(target_val[i]))
 
     def test_model(self):
         pass
@@ -78,33 +79,36 @@ class Agent:
             if state.ndim == 1:
                 state = np.array([state])
             q_learning_value = self.model.predict(state)
-            print(np.argmax(q_learning_value[0]))
             return np.argmax(q_learning_value[0])
+
 
 EPISODES = 3000
 env = gym_tetris.make('TetrisA-v0')
 env = JoypadSpace(env, MOVEMENT)
 cv2.namedWindow('ComWin', cv2.WINDOW_NORMAL)
 
-state_input_size = env.observation_space.shape[0] * env.observation_space.shape[1]
+state_input_size = int((env.observation_space.shape[0] * \
+                   env.observation_space.shape[1])/64)
 number_of_actions = env.action_space.n
 agent = Agent(state_input_size, number_of_actions)
 done = True
 
+inx, iny, inc = env.observation_space.shape
+inx = int(inx / 8)
+iny = int(iny / 8)
 for e in range(EPISODES):
     score = 0
     if done:
         state = env.reset()
-    grayimg = cv2.cvtColor(state, cv2.COLOR_RGB2GRAY)
-    #grayimg = np.reshape(grayimg, [256, state_input_size])
+    env.render()
+    grayimg = cv2.resize(state, (inx, iny))
+    grayimg = cv2.cvtColor(grayimg, cv2.COLOR_RGB2GRAY)
+    # grayimg = np.reshape(grayimg, [256, state_input_size])
+    #grayimg = np.reshape(grayimg, (inx, iny))
     cv2.imshow('ComWin', grayimg)
     grayimg = np.ndarray.flatten(grayimg)
-    env.render()
+
     action = agent.get_action(grayimg)
     state, reward, done, info = env.step(action)
 
-
 env.close()
-
-
-
