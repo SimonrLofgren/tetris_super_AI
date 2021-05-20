f96af5aaa144c4958023db1bbe72a1f8f94b5399
Simon Löfgren <simonrlofgren@hotmail.com>
2021-05-20 15:09:52 +0200

    fitness patch 0.6.3 live stats fixed

--- analys.py
+++ analys.py
@@ -1,5 +1,7 @@
 from statistics import Statistics
-
+import pandas as pd
+import seaborn
+import matplotlib.pyplot as plt
 
 def main():
     running = True
@@ -7,6 +9,7 @@ def main():
         print('1. one')
         print('2. two')
         print('3. mean')
+        print('4. Live updates')
         inp = input('-: ')
         if inp == 'exit':
             running = False
@@ -40,6 +43,43 @@ def main():
                 Statistics.plot(mean, ymax=0.5, xlabel='episode', ylabel='Mean_time')
             except:
                 print('not valid')
+
+        elif inp == '4':
+            print('which graph?')
+            print('1. Total score')
+            print('2. Average score')
+            print('3. Average score last 10')
+
+            inp = input('-: ')
+
+            if inp == '1':
+                try:
+                    csv = pd.read_csv(r'scores.csv')
+                    res = seaborn.lineplot(x="Episode", y="Total Score", data=csv)
+                    plt.show()
+                except Exception as e:
+                    print(e)
+                    print('not valid')
+
+            if inp == '2':
+                try:
+                    csv = pd.read_csv(r'scores.csv')
+                    res = seaborn.lineplot(x="Episode", y="Average Score", data=csv)
+                    plt.show()
+                except Exception as e:
+                    print(e)
+                    print('not valid')
+
+            if inp == '3':
+                try:
+                    csv = pd.read_csv(r'scores.csv')
+                    res = seaborn.lineplot(x="Episode", y="Average Score Last 10", data=csv)
+                    plt.show()
+                except Exception as e:
+                    print(e)
+                    print('not valid')
+            else:
+                pass
 
         else:
             try:
